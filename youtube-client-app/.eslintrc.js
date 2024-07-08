70b72a041d2b17b889cc4e507754b7535f206d57
Anna Golosova <a.golosova@3l.ru>
2024-07-08 12:56:12 +0300

    feat: setting eslint Angular project

--- youtube-client-app/.eslintrc.js
+++ youtube-client-app/.eslintrc.js
@@ -1,26 +1,69 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  ignorePatterns: ['.eslintrc.js'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['@typescript-eslint', '@angular-eslint/template'],
+  extends: [
+    'plugin:@angular-eslint/recommended',
+    'plugin:@angular-eslint/template/process-inline-templates',
+    'eslint-config-airbnb-base',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['*.ts'],
+      parserOptions: {},
+      rules: {
+        'comma-dangle': 'off', // Отключить базовое правило ESLint
+        '@typescript-eslint/comma-dangle': ['error', 'always-multiline'], // Включить для TypeScript
+      },
     },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    {
+      files: ['*.html'],
+      parser: '@angular-eslint/template-parser',
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {},
+    },
+    {
+      files: ['*.spec.ts'],
+      env: {
+        jasmine: true,
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "rules": {
-    }
-}
+    'import/resolver': {
+      typescript: { alwaysTryTypes: true },
+      node: {
+        extensions: ['.js', '.jsx', '.json', '.scss', '.html', '.ts', '.tsx'],
+      },
+    },
+  },
+};
