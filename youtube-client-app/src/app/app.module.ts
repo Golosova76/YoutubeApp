86dc82bb1d00eaa9462b981331bf387cf9825f06
Anna Golosova <a.golosova@3l.ru>
2024-07-08 15:58:21 +0300

    feat: add components Angular project

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppComponent from './app.component';
 import AppRoutingModule from './app-routing.module';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SearchBarComponent } from './features/search/search-bar/search-bar.component';
+import { SearchResultsComponent } from './features/search/search-results/search-results.component';
+import { SearchItemComponent } from './features/search/search-item/search-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, SearchBarComponent, SearchResultsComponent, SearchItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
