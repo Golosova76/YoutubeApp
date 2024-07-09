f1df116c1a55adb92967429e6a34e8d1de80985b
Anna Golosova <a.golosova@3l.ru>
2024-07-09 16:24:54 +0300

    feat: add search input

--- youtube-client-app/src/app/core/header/header.component.ts
+++ youtube-client-app/src/app/core/header/header.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { InputComponent } from 'app/shared/input/input.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, InputComponent],
 })
 export class HeaderComponent {}
---
+++ youtube-client-app/src/app/shared/input/input.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+})
+export class InputComponent {
+  @Input() placeholder: string = '';
+
+  @Input() type: string = 'text';
+
+  @Input() label: string = '';
+}
