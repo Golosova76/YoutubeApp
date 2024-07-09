62cd467ec7748705974f7c9a542cc49bc105a52c
Anna Golosova <a.golosova@3l.ru>
2024-07-09 19:14:19 +0300

    feat: add search button

--- youtube-client-app/src/app/core/header/header.component.ts
+++ youtube-client-app/src/app/core/header/header.component.ts
@@ -1,13 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { InputComponent } from 'app/shared/input/input.component';
+import { CustomButtonComponent } from 'app/shared/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, InputComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    InputComponent,
+    MatIconModule,
+    CustomButtonComponent,
+  ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  // eslint-disable-next-line class-methods-use-this
+  onSearch() {
+    console.log('Search initiated');
+    // Тут может быть реализация логики поиска или вызов сервиса
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleButtonClick() {}
+}
---
+++ youtube-client-app/src/app/shared/custom-button/custom-button.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-custom-button',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.scss',
+})
+export class CustomButtonComponent {}
