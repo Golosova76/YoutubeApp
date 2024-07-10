3ae0490da32512bd8b5fe90a6bf15f6db7d7984b
Anna Golosova <a.golosova@3l.ru>
2024-07-10 11:21:18 +0300

    feat: update angular.json

--- youtube-client-app/src/main.ts
+++ youtube-client-app/src/main.ts
@@ -1,8 +1,12 @@
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from 'app/app-routing.module';
 import { AppComponent } from 'app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(AppRoutingModule)],
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 }).catch((err) => console.error(err));
