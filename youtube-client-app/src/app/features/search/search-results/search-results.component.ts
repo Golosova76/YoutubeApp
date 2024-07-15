4de7c346bab442f7e318cb2b07b5ab590aaba4ce
Anna Golosova <a.golosova@3l.ru>
2024-07-15 16:38:13 +0300

    feat: add object videosData

--- youtube-client-app/src/app/features/search/search-results/search-results.component.ts
+++ youtube-client-app/src/app/features/search/search-results/search-results.component.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import {
+  createVideosData,
+  YouTubeVideoListResponse,
+} from 'app/shared/models/search-response.model';
 import { SearchItemComponent } from './search-item/search-item.component';
 
 @Component({
@@ -9,4 +14,22 @@ import { SearchItemComponent } from './search-item/search-item.component';
   standalone: true,
   imports: [CommonModule, SearchItemComponent],
 })
-export class SearchResultsComponent {}
+export class SearchResultsComponent {
+  public videosData!: YouTubeVideoListResponse;
+
+  async loadData() {
+    // eslint-disable-next-line operator-linebreak
+    const url =
+      'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/angular/response.json';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const responseData = await response.json();
+      this.videosData = createVideosData(responseData);
+    } catch (error) {
+      console.error('Error loading the videos', error);
+    }
+  }
+}
--- youtube-client-app/src/app/shared/models/search-response.model.ts
+++ youtube-client-app/src/app/shared/models/search-response.model.ts
@@ -1,5 +1,3 @@
-// export default class SearchResponse {}
-
 import { VideoItem } from './search-item.model';
 
 export interface PageInfo {
@@ -13,3 +11,51 @@ export interface YouTubeVideoListResponse {
   pageInfo: PageInfo;
   items: VideoItem[];
 }
+
+export const createVideosData = (
+  // eslint-disable-next-line @typescript-eslint/comma-dangle
+  dataResponse: YouTubeVideoListResponse
+): YouTubeVideoListResponse => {
+  // const kind = dataResponse.kind;
+  const { kind, etag } = dataResponse;
+
+  const { totalResults, resultsPerPage } = dataResponse.pageInfo;
+  const pageInfo: PageInfo = { totalResults, resultsPerPage };
+
+  // Массив обработанных элементов (VideoItem)
+  const items: VideoItem[] = dataResponse.items.map((item) => ({
+    kind: item.kind,
+    etag: item.etag,
+    id: item.id,
+    snippet: {
+      publishedAt: item.snippet.publishedAt,
+      channelId: item.snippet.channelId,
+      title: item.snippet.title,
+      description: item.snippet.description,
+      thumbnails: item.snippet.thumbnails,
+      channelTitle: item.snippet.channelTitle,
+      tags: item.snippet.tags,
+      categoryId: item.snippet.categoryId,
+      liveBroadcastContent: item.snippet.liveBroadcastContent,
+      localized: item.snippet.localized,
+      defaultAudioLanguage: item.snippet.defaultAudioLanguage,
+    },
+    statistics: {
+      viewCount: item.statistics.viewCount,
+      likeCount: item.statistics.likeCount,
+      dislikeCount: item.statistics.dislikeCount,
+      favoriteCount: item.statistics.favoriteCount,
+      commentCount: item.statistics.commentCount,
+    },
+  }));
+
+  // Создаем объект YouTubeVideoListResponse
+  const videosData: YouTubeVideoListResponse = {
+    kind,
+    etag,
+    pageInfo,
+    items,
+  };
+
+  return videosData;
+};
