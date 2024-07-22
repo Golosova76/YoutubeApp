4a1065721fff1a20c9c8027c6a567559d5fd68bd
Anna Golosova <a.golosova@3l.ru>
2024-07-22 16:31:09 +0300

    feat: add page detailed-information

---
+++ youtube-client-app/src/app/youtube/pages/detailed-information/detailed-information.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './detailed-information.component.html',
+  styleUrls: ['./detailed-information.component.scss'],
+})
+export class DetailedInformationComponent {}
--- youtube-client-app/src/app/youtube/youtube-routing.module.ts
+++ youtube-client-app/src/app/youtube/youtube-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchResultsComponent } from './pages/search-results/search-results.component';
+import { DetailedInformationComponent } from './pages/detailed-information/detailed-information.component';
 
-const routes: Routes = [{ path: '', component: SearchResultsComponent }];
+const routes: Routes = [
+  { path: '', component: SearchResultsComponent },
+  { path: '', component: DetailedInformationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
