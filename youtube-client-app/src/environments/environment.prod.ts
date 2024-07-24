4e8772718e63fbbd3f380b90e7524630c987096d
Anna Golosova <a.golosova@3l.ru>
2024-07-24 14:11:26 +0300

    feat: add youtube-api service

---
+++ youtube-client-app/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  youtubeApiKey: 'AIzaSyB6yH3mcD8S-c87Ht-hPWHIJBl2tj8ZUoY',
+  youtubeSearchUrl: 'https://www.googleapis.com/youtube/v3/search',
+  youtubeVideosUrl: 'https://www.googleapis.com/youtube/v3/videos',
+};
---
+++ youtube-client-app/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  youtubeApiKey: 'AIzaSyB6yH3mcD8S-c87Ht-hPWHIJBl2tj8ZUoY',
+  youtubeSearchUrl: 'https://www.googleapis.com/youtube/v3/search',
+  youtubeVideosUrl: 'https://www.googleapis.com/youtube/v3/videos',
+};
